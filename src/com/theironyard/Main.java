df4c769ad9371e7c497883e9a86de7618d63d29f
willriggins <williamriggins2013@gmail.com>
2016-06-08 11:57:41 -0400

    post new msgs

--- src/com/theironyard/Main.java
+++ src/com/theironyard/Main.java
@@ -1,5 +1,10 @@
 package com.theironyard;
 
+import spark.ModelAndView;
+import spark.Session;
+import spark.Spark;
+import spark.template.mustache.MustacheTemplateEngine;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 
@@ -11,6 +16,86 @@ public class Main {
     public static void main(String[] args) {
 	    addTestUsers();
         addTestMessages();
+
+        Spark.init(); // not necessary, but zach usually puts in through habit
+
+        Spark.get(
+                "/",
+                (request, response) -> {
+                    Session session = request.session();
+                    String username = session.attribute("username");
+
+                    String idStr = request.queryParams("replyId");
+                    int replyId = -1;
+                    if (idStr != null) {
+                        replyId = Integer.valueOf(idStr);
+                    }
+
+
+                    ArrayList<Message> subset = new ArrayList<Message>();
+                    for (Message msg : messages) {
+                        if (msg.replyId == replyId) {
+                            subset.add(msg);
+                        }
+                    }
+
+                    HashMap m = new HashMap();
+                    m.put("messages", subset);
+                    m.put("username", username);
+                    m.put("replyId", replyId);
+                    return new ModelAndView(m, "home.html");
+                },
+                new MustacheTemplateEngine()
+        );
+        Spark.post(
+                "/login",
+                (request, response) -> {
+                    String username = request.queryParams("username");
+                    if (username == null) {
+                        throw new Exception("Login name not found");
+                    }
+
+                    User user = users.get(username);
+                    if (user == null) {
+                        user = new User(username, "");
+                        users.put(username, user);
+                    }
+
+                    Session session = request.session();
+                    session.attribute("username", username);
+
+                    response.redirect(request.headers("Referer"));
+                    return "";
+                }
+        );
+        Spark.post(
+                "/logout",
+                (request, response) -> {
+                    Session session = request.session();
+                    session.invalidate();
+                    response.redirect("/");
+                    return "";
+                }
+        );
+        Spark.post(
+                "/create-message",
+                (request, response) -> {
+                    Session session = request.session();
+                    String username = session.attribute("username");
+                    if (username == null) {
+                        throw new Exception("Not logged in");
+                    }
+
+                    int replyId = Integer.valueOf(request.queryParams("replyId"));
+                    String text = request.queryParams("message");
+
+                    Message msg = new Message(messages.size(), replyId, username, text);
+                    messages.add(msg);
+
+                    response.redirect(request.headers("Referer"));
+                    return "";
+                }
+        );
     }
 
     static void addTestUsers() {
