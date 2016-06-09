04fde208ae9b07a82bc618546af406a301f8993e
willriggins <williamriggins2013@gmail.com>
2016-06-09 12:12:10 -0400

    delete sytem

--- src/com/theironyard/Main.java
+++ src/com/theironyard/Main.java
@@ -39,10 +39,17 @@ public class Main {
                         }
                     }
 
+                    Message parentMsg = null;
+                    if (replyId >= 0) {
+                        parentMsg = messages.get(replyId);
+                    }
+
                     HashMap m = new HashMap();
                     m.put("messages", subset);
                     m.put("username", username);
                     m.put("replyId", replyId);
+                    m.put("message", parentMsg);
+                    m.put("isMe", parentMsg != null && username != null && parentMsg.author.equals(username));
                     return new ModelAndView(m, "home.html");
                 },
                 new MustacheTemplateEngine()
@@ -93,6 +100,30 @@ public class Main {
                     messages.add(msg);
 
                     response.redirect(request.headers("Referer"));
+                    return "";
+                }
+        );
+        Spark.post(
+                "/delete-message",
+                (request, response) -> {
+                    int id = Integer.valueOf(request.queryParams("id"));
+
+                    Session session = request.session();
+                    String username = session.attribute("username");
+
+                    Message m = messages.get(id);
+                    if (!m.author.equals(username)) {
+                        throw new Exception("You can't delete this!");
+                    }
+
+                    messages.remove(id);
+                    // reset ids
+                    int index = 0;
+                    for (Message msg: messages) {
+                        msg.id = index;
+                        index++;
+                    }
+                    response.redirect("/");
                     return "";
                 }
         );
